03db7130499dbda066c4a0c5827e5a783749b623
Izma Raifani <izurai@gmail.com>
2024-07-02 16:23:44 +0700

    ..

--- src/app/ui/map.js
+++ src/app/ui/map.js
@@ -12,16 +12,18 @@ export default function MyMap ({position}){
 
   return (
     
-    <MapContainer center={position} zoom={13} style={{ height: '320px', width:'375px' }}>
-      <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker position={position}>
-          {/* <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup> */}
-      </Marker>
-  </MapContainer>
+  //   <MapContainer center={position} zoom={13} style={{ height: '320px', width:'375px' }}>
+  //     <TileLayer
+  //         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  //         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  //     />
+  //     <Marker position={position}>
+  //         {/* <Popup>
+  //             A pretty CSS3 popup. <br /> Easily customizable.
+  //         </Popup> */}
+  //     </Marker>
+  // </MapContainer>
+
+  <h1>nothing here</h1>
   )
 }
